4cc82c0d3419b08719da27a893f4def5c7e375da
José Luis Fernández-Mayoralas <yo@jluis.me>
2019-01-10 13:49:04 +0100

    Adds blank project files

---
+++ Socializa/Socializa/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Socializa
+//
+//  Created by Jose Luis Fernandez-Mayoralas on 10/1/19.
+//  Copyright © 2019 wadobo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
